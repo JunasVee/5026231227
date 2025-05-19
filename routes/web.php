6006df648ff122b8c434fbc27285630a53d60d91
JunasVee <arjunagamers777@gmail.com>
2025-05-20 00:12:58 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+// routes/web.php
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,10 +9,31 @@ Route::get('/', function () {
 });
 
 Route::get('halo', function () {
-	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+    return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
 });
-
 
 Route::get('blog', function () {
-	return view('blog');
+    return view('blog');
 });
+
+// ─── Your new routes ────────────────────────────────────
+// Assuming you have:
+//   resources/views/input.blade.php
+//   resources/views/js1.blade.php
+//   resources/views/js2.blade.php
+
+Route::get('input', function () {
+    return view('input');
+})->name('input');
+
+Route::get('js1', function () {
+    return view('js1');
+})->name('js1');
+
+Route::get('js2', function () {
+    return view('js2');
+})->name('js2');
+
+Route::get('bootstrap2', function () {
+    return view('bootstrap2');
+})->name('bootstrap2');
